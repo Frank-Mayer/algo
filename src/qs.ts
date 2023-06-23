a503c2c70d8784aa641c66d2045d516a624b2b63
Frank Mayer <git@frank-mayer.io>
2023-06-23 16:56:16 +0200

    format

--- src/qs.ts
+++ src/qs.ts
@@ -12,7 +12,7 @@ function qs(arr: Array<number>, lo: number, hi: number): void {
 function partition(arr: Array<number>, lo: number, hi: number): number {
     const pivot = arr[hi];
     let idx = lo - 1;
-    for(let i = lo; i < hi; ++i) {
+    for (let i = lo; i < hi; ++i) {
         if (arr[i] <= pivot) {
             ++idx;
             const tmp = arr[idx];
